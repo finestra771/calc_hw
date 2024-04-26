3e559b7360877073c35a36df69d02e8a84b60c23
조현아 <minjokwindows5@gmail.com>
2024-04-26 11:42:29 +0900

    요구사항 3

--- src/Calc.java
+++ src/Calc.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Calc {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
         System.out.print("첫 번째 숫자를 입력하세요: ");
         int firstNum = sc.nextInt();
@@ -11,5 +11,23 @@ public class Calc {
 
         System.out.print("사칙연산 기호를 입력하세요: ");
         char operator = sc.next().charAt(0);
+        int result = 0;
+        switch (operator) {
+            case '+':
+                result = firstNum + secondNum;
+                break;
+            case '-':
+                result = firstNum - secondNum;
+                break;
+            case '*':
+                result = firstNum * secondNum;
+                break;
+            case '/':
+                if (secondNum != 0) result = firstNum / secondNum;
+                else {
+                    System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
+                    result = 0;
+                }
+        }
     }
 }
